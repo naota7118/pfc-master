712f330d9f8c4ad39994144ac435f6d0d2c31588
naota7118 <naota7118@gmail.com>
2020-05-05 00:32:32 +0900

    input example foods

--- db/migrate/20200504144642_create_posts.rb
+++ db/migrate/20200504144642_create_posts.rb
@@ -1,16 +1,16 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-      t.interger :food,    null: false
-      t.interger :calorie  
-      t.interger :protein 
-      t.interger :fat 
-      t.interger :carbo 
+      t.text :food, null: false
+      t.integer :calorie  
+      t.integer :protein 
+      t.integer :fat 
+      t.integer :carbo 
       t.text :text
-      t.image :text
-      t.date :date         null: false
-      .refereces :user,    forein_key: true
-      t.timestamps         null: false
+      t.text :image
+      t.date :date
+      t.references :user, foreign_key: true
+      t.timestamps
     end
   end
 end
