020b7827635c503e1ce8fdf5246ea67e8975be80
naota7118 <naota7118@gmail.com>
2020-05-05 10:27:20 +0900

    add new_action to post_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,10 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
+
+  def new
+    @posts = Post.new
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :posts, only: :index
+  resources :posts, only: [:index, :new]
 end
