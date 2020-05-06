6804fd83688b812863f86ef647357ee97bcddced
naota7118 <naota7118@gmail.com>
2020-05-06 13:27:02 +0900

    change form_with in posts.new.html.haml

--- Gemfile
+++ Gemfile
@@ -16,7 +16,6 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'spring'
   gem 'pry-rails'
-  gem 'compass-rails'
   gem 'sprockets'
   gem 'kaminari'
   gem 'devise'
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,7 +14,7 @@ class PostsController < ApplicationController
   
   private
   def post_params
-    params.require(:post).permit(:title, :content)
+    params.require(:post).permit(:food, :calorie, :protein, :fat, :carbo, :content, :image)
   end
 
 end
