7222efa14bf9ff06aa6abda37fdafdeba28435d5
naota7118 <naota7118@gmail.com>
2020-05-14 17:10:06 +0900

    remake post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_14_042245) do
+ActiveRecord::Schema.define(version: 2020_05_14_080622) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "comment", null: false
@@ -41,17 +41,14 @@ ActiveRecord::Schema.define(version: 2020_05_14_042245) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "food", null: false
-    t.integer "calorie"
+    t.integer "calorie", null: false
     t.integer "protein"
     t.integer "fat"
     t.integer "carbo"
     t.text "text"
-    t.text "image"
-    t.date "date"
-    t.bigint "user_id"
+    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -69,5 +66,4 @@ ActiveRecord::Schema.define(version: 2020_05_14_042245) do
   add_foreign_key "images", "posts"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
-  add_foreign_key "posts", "users"
 end
