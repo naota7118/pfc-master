bc409fefbbec8ee74ff57788cf41312a197b456c
naota7118 <naota7118@gmail.com>
2020-05-17 23:39:31 +0900

    edit user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: "posts#index"
   resources :posts, only: [:new, :create, :show, :edit]
+  resources :users, only: [:edit, :update]
+
 end
