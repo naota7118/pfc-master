4be98e4537daa1885af0d1faee1d7f8869452e73
naota7118 <naota7118@gmail.com>
2020-05-18 06:36:37 +0900

    add column name to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_14_080622) do
+ActiveRecord::Schema.define(version: 2020_05_17_211741) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "comment", null: false
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2020_05_14_080622) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   add_foreign_key "comments", "posts"
