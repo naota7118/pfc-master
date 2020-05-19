32422410b4f36bf3c8b263b8ae637f58a564c73a
naota7118 <naota7118@gmail.com>
2020-05-19 09:57:26 +0900

    change from erb  to haml

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   root to: "posts#index"
   resources :posts, only: [:new, :create, :show, :edit]
-  resources :users, only: [:edit, :update]
+  resources :users
 
 end
