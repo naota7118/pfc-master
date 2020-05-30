fc34cad1610a8ba953cb346f894a625f4e7898cc
naota7118 <naota7118@gmail.com>
2020-05-30 22:15:26 +0900

    edit route.rb add post destroy and update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "posts#index"
-  resources :posts, only: [:new, :create, :show, :edit]
+  resources :posts, only: [:new, :create, :show, :edit, :destroy, :update]
 end
