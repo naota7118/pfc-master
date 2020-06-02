afd7340d3c9476ed649a976afc6d8d589b7299c3
naota7118 <naota7118@gmail.com>
2020-06-03 08:58:18 +0900

    edit destroy action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -30,9 +30,13 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
-    @post.destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_back(fallback_location: root_path) 
     # 削除ボタンを押したら、「id=23が見つかりません」と出てきた。なぜ？
+    # 削除ボタンを押したら、sequel proでデータは消えていた。これがid=23が見つかりませんの原因だった。
+    # データは削除されているのに、画面が変わらないのはなぜ？
+    # 削除した後の記述をしてないからか。redirect_back root_pathを書き足した。その結果、削除が完了した後の画面になった。
   end
 
   def update
