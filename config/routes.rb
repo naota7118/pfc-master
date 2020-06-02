663bdc095580185a802bea8d2cb20f59a5af3161
naota7118 <naota7118@gmail.com>
2020-06-03 08:57:45 +0900

    edit route post

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   
   resources :users, only: [:index, :show]
-  resources :posts # only: [:index, :new, :create, :show, :edit, :destroy, :update] 7つ全部使うので消した
-
+  resources :posts
+  
   root to: "posts#index"
 end
