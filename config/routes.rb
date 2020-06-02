e127b635d437f2cb6acd516300f5f858c67495ee
naota7118 <naota7118@gmail.com>
2020-06-03 07:59:55 +0900

    edit route about post action

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   
   resources :users, only: [:index, :show]
-  resources :posts, only: [:index, :new, :create, :show, :edit, :destroy, :update]
+  resources :posts # only: [:index, :new, :create, :show, :edit, :destroy, :update] 7つ全部使うので消した
 
   root to: "posts#index"
 end
