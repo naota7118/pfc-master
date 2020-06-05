15f8041eb5efe4b3e1722c02f1b6f1256d92f4df
naota7118 <naota7118@gmail.com>
2020-06-06 06:29:39 +0900

    add comments in route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  
-  resources :users, only: [:index, :show]
-  resources :posts
-  
   root to: "posts#index"
+  resources :posts do
+    resources :comments, only: :create
+  end
+  resources :users, only: [:index, :show]
 end
