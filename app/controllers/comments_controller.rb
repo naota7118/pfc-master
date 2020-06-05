11362b5addf76c1e7ee3ab5103afceb98d7dd83a
naota7118 <naota7118@gmail.com>
2020-06-06 06:48:47 +0900

    edit comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,17 @@
 class CommentsController < ApplicationController
+  def create
+    # comment = Comment.create(comment_params) # createだと保存に成功したか失敗したかがわからない
+    comment = Comment.new(comment_params) 
+    if comment.save
+      redirect_to post_path(comment.post.id) # コメントと結びつく投稿の詳細画面に遷移する
+    else
+      flash[:alert] = 'エラーが発生しました。'
+      redirect_to post_path(comment.post.id)
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:comment).merge(user_id: current_user.id, post_id: params[:post_id])
+  end
 end
