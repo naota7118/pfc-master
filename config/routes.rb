5fa3a5282fa2906b436cf32b96a0a66421d7dc35
naota7118 <naota7118@gmail.com>
2020-06-13 23:39:25 +0900

    create follow button

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
     resources :likes, only: [:create, :destroy]
   end
   resources :users, only: [:index, :show]
+  resources :relationships, only: [:create, :destroy]
 end
