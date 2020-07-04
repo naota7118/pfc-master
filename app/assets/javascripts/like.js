2788ed9d08c2a4e6bc69d971d7b420a0f93c6c77
naota7118 <naota7118@gmail.com>
2020-07-04 23:10:54 +0900

    rename like.js

---
+++ app/assets/javascripts/like.js
@@ -0,0 +1,51 @@
+$(function(){
+  
+  function buildHTML(){
+    var html = `<form class="button_to" method="post" action="/posts/21/likes/21">
+                  <input type="hidden" name="_method" value="delete">
+                  <input type="submit" value="いいねを取り消す">
+                  <input type="hidden" name="authenticity_token" value="UvQ++DpAnTnzOzUfo8aJgIoezBCtRzo4KAYh0IsUXftYw0AmHcH8DY+fgOeJqbuFsI2EwP9lXicWlP9rsyAtpQ==">
+                </form>`
+    return html;
+  }
+
+  $('.button_to').on('submit', function(e) {
+    e.preventDefault();
+    if($('.button_to').children().is('#like')) {
+      console.log(this);
+      var formData = new FormData(this);
+      var url = $(this).attr('action')
+      $.ajax({
+        url: url,
+        type: "POST",
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      }) 
+      
+    } else {
+      console.log('dislike');
+      alert('エラー');
+    }
+    // var formData = new FormData(this);
+    // var url = $(this).attr('action');
+    // $.ajax({
+    //   url: url,
+    //   type: "POST",
+    //   data: formData,
+    //   dataType: 'json',
+    //   processData: false,
+    //   contentType: false
+    // })
+    // .done(function(data){
+    //   var html = buildHTML(data);
+    //   $('.comments').prepend(html);
+    //   $('#comment_comment').val('');
+    //   $('#commentBtn').prop('disabled', false);
+    // })
+    // .fail(function(){
+    //   alert('エラー');
+    // })
+  })
+})
