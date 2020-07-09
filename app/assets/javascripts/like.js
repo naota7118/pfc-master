533fd8500bb393f9f01de2b75abc40350be86006
naota7118 <naota7118@gmail.com>
2020-07-09 19:58:02 +0900

    setting like function

--- app/assets/javascripts/like.js
+++ app/assets/javascripts/like.js
@@ -1,90 +1,90 @@
-$(function(){
+// $(function(){
   
-  function buildHTML(like){
-    var html = `<form class="button_to" method="post" action="/posts/${like.post_id}/likes/${like.id}">
-                  <input type="hidden" name="_method" value="delete">
-                  <input type="submit" value="いいねを取り消す" id="delete">
-                </form>
-                <div class="likeCounts">
-                  いいね数：
-                  ${like.counts}
-                </div>`
-    return html;
-  }
+//   function buildHTML(like){
+//     var html = `<form class="button_to" method="post" action="/posts/${like.post_id}/likes/${like.id}">
+//                   <input type="hidden" name="_method" value="delete">
+//                   <input type="submit" value="いいねを取り消す" id="delete">
+//                 </form>
+//                 <div class="likeCounts">
+//                   いいね数：
+//                   ${like.counts}
+//                 </div>`
+//     return html;
+//   }
 
-  function buildDeleteHTML(like){
-    var html = `<form class="button_to" method="post" action="/posts/${like.post_id}/likes">
-                  <input type="hidden" name="_method" value="post">
-                  <input type="submit" value="いいね" id="like">
-                </form>
-                <div class="likeCounts">
-                  いいね数：
-                  ${like.counts}
-                </div>`
-    return html;
-  }
+//   function buildDeleteHTML(like){
+//     var html = `<form class="button_to" method="post" action="/posts/${like.post_id}/likes">
+//                   <input type="hidden" name="_method" value="post">
+//                   <input type="submit" value="いいね" id="like">
+//                 </form>
+//                 <div class="likeCounts">
+//                   いいね数：
+//                   ${like.counts}
+//                 </div>`
+//     return html;
+//   }
   
-  $('.button_to').on('submit', function(e) {
-    e.preventDefault();
-    $.ajaxSetup({
-      beforeSend: function(xhr) {
-        xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'));
-        console.log('aaaaa');
-      }
-    });
-    // $.ajaxPrefilter(function(options, originalOptions, jqXHR) {
-    //   var token;
-    //   if (!options.crossDomain) {
-    //     token = $('meta[name="csrf-token"]').attr('content');
-    //     if (token) {
-    //       return jqXHR.setRequestHeader('X-CSRF-Token', token);
-    //     }
-    //   }
-    // });
-    if($('.button_to').children().is('#like')) {
-      var formData = new FormData(this);
-      var url = $(this).attr('action')
-      $.ajax({
-        url: url,
-        type: "POST",
-        data: formData,
-        dataType: 'json',
-        processData: false,
-        contentType: false
-      }) 
-      .done(function(data){
-        $('.button_to').remove();
-        $('.likeCounts').remove();
-        var html = buildHTML(data);
-        $('.like').append(html);
-        console.log('good');
-        $('.button_to').prop('disabled', false);
-      })
-      .fail(function(){
-        alert('エラー');
-      })
-    } else {
-      var formData = new FormData(this);
-      console.log(this);
-      var url = $(this).attr('action')
-      $.ajax({
-        url: url,
-        type: "DELETE",
-        data: formData,
-        dataType: 'json',
-        processData: false,
-        contentType: false
-      }) 
-      .done(function(data){
-        $('.button_to').remove();
-        $('.likeCounts').remove();
-        var html = buildDeleteHTML(data);
-        $('.like').append(html);
-        $('.button_to').prop('disabled', false);
-      })
-      .fail(function(){
-        alert('エラー');
-      })
-    }
-  })
-})
+//   $('.button_to').on('submit', function(e) {
+//     e.preventDefault();
+//     $.ajaxSetup({
+//       beforeSend: function(xhr) {
+//         xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'));
+//         console.log('aaaaa');
+//       }
+//     });
+//     // $.ajaxPrefilter(function(options, originalOptions, jqXHR) {
+//     //   var token;
+//     //   if (!options.crossDomain) {
+//     //     token = $('meta[name="csrf-token"]').attr('content');
+//     //     if (token) {
+//     //       return jqXHR.setRequestHeader('X-CSRF-Token', token);
+//     //     }
+//     //   }
+//     // });
+//     if($('.button_to').children().is('#like')) {
+//       var formData = new FormData(this);
+//       var url = $(this).attr('action')
+//       $.ajax({
+//         url: url,
+//         type: "POST",
+//         data: formData,
+//         dataType: 'json',
+//         processData: false,
+//         contentType: false
+//       }) 
+//       .done(function(data){
+//         $('.button_to').remove();
+//         $('.likeCounts').remove();
+//         var html = buildHTML(data);
+//         $('.like').append(html);
+//         console.log('good');
+//         $('.button_to').prop('disabled', false);
+//       })
+//       .fail(function(){
+//         alert('エラー');
+//       })
+//     } else {
+//       var formData = new FormData(this);
+//       console.log(this);
+//       var url = $(this).attr('action')
+//       $.ajax({
+//         url: url,
+//         type: "DELETE",
+//         data: formData,
+//         dataType: 'json',
+//         processData: false,
+//         contentType: false
+//       }) 
+//       .done(function(data){
+//         $('.button_to').remove();
+//         $('.likeCounts').remove();
+//         var html = buildDeleteHTML(data);
+//         $('.like').append(html);
+//         $('.button_to').prop('disabled', false);
+//       })
+//       .fail(function(){
+//         alert('エラー');
+//       })
+//     }
+//   })
+// })
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,34 +1,47 @@
 class LikesController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
 
   def create
-    # binding.pry
-    @like = current_user.likes.create(post_id: params[:post_id])
-    if @like.save
-      @likeCounts = Like.where(post_id: params[:post_id])
-      respond_to do |format|
-        # format.html { redirect_to post_path(@like.post.id) }
-        format.json
-      end
-      # binding.pry
-    else
-      flash[:alert] = 'エラーが発生しました。'
-      redirect_to post_path(@like.post.id)
-    end
+    @post = Post.find(params[:post_id])
+    @like = current_user.likes.build(post_id: params[:post_id])
+    @like.save
+    @likeCounts = Like.where(post_id: params[:post_id])
   end
 
   def destroy
+    @post = Post.find(params[:post_id])
     @like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
     @like.destroy
-    if @like.destroy
-      @likeCounts = Like.where(post_id: params[:post_id])
-      respond_to do |format|
-        # format.html { redirect_to post_path(@like.post.id) }
-        format.json
-      end
-    else
-      flash[:alert] = 'エラーが発生しました。'
-      redirect_to post_path(@like.post.id)
-    end
+    @likeCounts = Like.where(post_id: params[:post_id])
   end
+
+  # def create
+  #   # binding.pry
+  #   @like = current_user.likes.create(post_id: params[:post_id])
+  #   if @like.save
+  #     @likeCounts = Like.where(post_id: params[:post_id])
+  #     respond_to do |format|
+  #       # format.html { redirect_to post_path(@like.post.id) }
+  #       format.json
+  #     end
+  #     # binding.pry
+  #   else
+  #     flash[:alert] = 'エラーが発生しました。'
+  #     redirect_to post_path(@like.post.id)
+  #   end
+  # end
+
+  # def destroy
+  #   @like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
+  #   if @like.destroy
+  #     @likeCounts = Like.where(post_id: params[:post_id])
+  #     respond_to do |format|
+  #       # format.html { redirect_to post_path(@like.post.id) }
+  #       format.json
+  #     end
+  #   else
+  #     flash[:alert] = 'エラーが発生しました。'
+  #     redirect_to post_path(@like.post.id)
+  #   end
+  # end
 end
