70a451dd571827ea896a1753cb38ca819e765150
naota7118 <naota7118@gmail.com>
2020-07-11 08:52:39 +0900

    setting protein fat carbo

--- app/assets/javascripts/standard.js
+++ app/assets/javascripts/standard.js
@@ -5,14 +5,10 @@ $(function() {
 
   var $standard_weight = $('#standard_weight');
   var $leanBodyMass = $('#leanBodyMass');
-  var $standard_calorie = $('#standard_calorie');
+  var $calorie = $('#calorie');
   $standard_weight.on('input', function(event) {
     var standard_weight = $standard_weight.val();
     var standard_weight = Number(standard_weight);
-    // var leanBodyMass = standard_weight*0.8
-    // var standard_calorie = standard_weight*32
-    // $leanBodyMass.text(leanBodyMass);
-    // $standard_calorie.text(standard_calorie);
     var $standard_bodyFatPercentage = $('#standard_bodyFatPercentage');
     $standard_bodyFatPercentage.on('input', function(event) {
       var standard_bodyFatPercentage = $standard_bodyFatPercentage.val();
@@ -20,9 +16,23 @@ $(function() {
       var division_bodyFatPercentage = standard_bodyFatPercentage/100
       var difference_bodyFatPercentage = 1-division_bodyFatPercentage
       var leanBodyMass = standard_weight * difference_bodyFatPercentage
-      var standard_calorie = leanBodyMass*40
+      var calorie = leanBodyMass*40
       $leanBodyMass.text(leanBodyMass);
-      $standard_calorie.text(standard_calorie);
+      $calorie.text(calorie);
+
+      var $protein = $('#protein');
+      var protein = leanBodyMass * 2.5
+      $protein.text(protein);
+
+      var $fat = $('#fat');
+      var fat = calorie * 0.2 / 9
+      $fat.text(fat);
+
+      var proteinCalorie = protein * 4
+      var fatCalorie = calorie * 0.2
+      var $carbo = $('#carbo');
+      var carbo = (calorie - proteinCalorie - fatCalorie) / 4
+      $carbo.text(carbo);
     })
   })
 
