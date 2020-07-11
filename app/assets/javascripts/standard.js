b147e824d5f5498f9b346608079b7ac44ae16a42
naota7118 <naota7118@gmail.com>
2020-07-11 18:44:17 +0900

    change Math round to save

--- app/assets/javascripts/standard.js
+++ app/assets/javascripts/standard.js
@@ -14,28 +14,32 @@ $(function() {
       var division_bodyFatPercentage = standard_bodyFatPercentage/100
       var difference_bodyFatPercentage = 1-division_bodyFatPercentage
       var leanBodyMass = standard_weight * difference_bodyFatPercentage
-      var mathLeanBodyMass = Math.round(leanBodyMass*10) / 10
+      var mathLeanBodyMass = Math.round(leanBodyMass)
       var calorie = leanBodyMass*40
-      var mathCalorie = Math.round(calorie*10) / 10
+      var mathCalorie = Math.round(calorie)
       $('#standard_leanBodyMass').val(mathLeanBodyMass);
       $leanBodyMass.text(mathLeanBodyMass);
+      $('#standard_calorie').val(mathCalorie);
       $calorie.text(mathCalorie);
 
       var $protein = $('#protein');
       var protein = leanBodyMass * 2.5
-      var mathProtein = Math.round(protein*10) / 10
+      var mathProtein = Math.round(protein)
+      $('#standard_protein').val(mathProtein);
       $protein.text(mathProtein);
 
       var $fat = $('#fat');
       var fat = calorie * 0.2 / 9
-      var mathFat = Math.round(fat*10) / 10
+      var mathFat = Math.round(fat)
+      $('#standard_fat').val(mathFat);
       $fat.text(mathFat);
 
       var proteinCalorie = protein * 4
       var fatCalorie = calorie * 0.2
       var $carbo = $('#carbo');
       var carbo = (calorie - proteinCalorie - fatCalorie) / 4
-      var mathCarbo = Math.round(carbo*10) / 10
+      var mathCarbo = Math.round(carbo)
+      $('#standard_carbo').val(mathCarbo);
       $carbo.text(mathCarbo);
       
     $standard_bodyFatPercentage.on('input', function(event) {
@@ -44,28 +48,32 @@ $(function() {
       var division_bodyFatPercentage = standard_bodyFatPercentage/100
       var difference_bodyFatPercentage = 1-division_bodyFatPercentage
       var leanBodyMass = standard_weight * difference_bodyFatPercentage
-      var mathLeanBodyMass = Math.round(leanBodyMass*10) / 10
+      var mathLeanBodyMass = Math.round(leanBodyMass)
       var calorie = leanBodyMass*40
-      var mathCalorie = Math.round(calorie*10) / 10
+      var mathCalorie = Math.round(calorie)
       $('#standard_leanBodyMass').val(mathLeanBodyMass);
       $leanBodyMass.text(mathLeanBodyMass);
+      $('#standard_calorie').val(mathCalorie);
       $calorie.text(mathCalorie);
 
       var $protein = $('#protein');
       var protein = leanBodyMass * 2.5
-      var mathProtein = Math.round(protein*10) / 10
+      var mathProtein = Math.round(protein)
+      $('#standard_protein').val(mathProtein);
       $protein.text(mathProtein);
 
       var $fat = $('#fat');
       var fat = calorie * 0.2 / 9
-      var mathFat = Math.round(fat*10) / 10
+      var mathFat = Math.round(fat)
+      $('#standard_fat').val(mathFat);
       $fat.text(mathFat);
 
       var proteinCalorie = protein * 4
       var fatCalorie = calorie * 0.2
       var $carbo = $('#carbo');
       var carbo = (calorie - proteinCalorie - fatCalorie) / 4
-      var mathCarbo = Math.round(carbo*10) / 10
+      var mathCarbo = Math.round(carbo)
+      $('#standard_carbo').val(mathCarbo);
       $carbo.text(mathCarbo);
     })
   })
