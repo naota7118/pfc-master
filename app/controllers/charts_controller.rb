fd0987bc0ad3e720f89c02a0e9ff89addaf83aa1
naota7118 <naota7118@gmail.com>
2020-07-12 20:29:16 +0900

    edit index.html.erb

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,6 +1,10 @@
 class ChartsController < ApplicationController
 
   def index
+    gon.data = []
+    @post = Post.where("calorie > ?", 1)
+    gon.data << @post.select("calorie")
+    binding.pry
   end
   
 end
