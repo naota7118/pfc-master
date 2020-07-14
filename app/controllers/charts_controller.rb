07b55140ff417a6439c1adb2c2990c7aad78dec3
naota7118 <naota7118@gmail.com>
2020-07-15 00:47:51 +0900

    label change

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -5,6 +5,9 @@ class ChartsController < ApplicationController
     @posts = current_user.posts.select(:calorie).map{ |posts| posts.calorie}
     gon.data = @posts
     @dates = current_user.posts.select(:created_at).map{ |posts| posts.created_at}
+    @dates.each do |a|
+      gon.date = a.strftime("%Y年%m月%d日")
+    end
   end
   
 end
