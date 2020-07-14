a8fef0d8f5d332253e5fd35081e767bd6ffcfe99
naota7118 <naota7118@gmail.com>
2020-07-14 23:14:49 +0900

    get only created_at

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,8 +1,10 @@
 class ChartsController < ApplicationController
 
   def index
+    binding.pry
     @posts = current_user.posts.select(:calorie).map{ |posts| posts.calorie}
     gon.data = @posts
+    @dates = current_user.posts.select(:created_at).map{ |posts| posts.created_at}
   end
   
 end
