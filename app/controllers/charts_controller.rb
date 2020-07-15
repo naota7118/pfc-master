3f887f64f839993919d88c167200bfbc37dc751f
naota7118 <naota7118@gmail.com>
2020-07-15 12:37:01 +0900

    edit charts_controller.rb

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -2,12 +2,15 @@ class ChartsController < ApplicationController
 
   def index
     binding.pry
-    @posts = current_user.posts.select(:calorie).map{ |posts| posts.calorie}
-    gon.data = @posts
+    gon.data = []
+    gon.data = current_user.posts.select(:calorie).map{ |posts| posts.calorie}
+
+    gon.date = []
     @dates = current_user.posts.select(:created_at).map{ |posts| posts.created_at}
     @dates.each do |a|
-      gon.date = a.strftime("%Y年%m月%d日")
+      gon.date << a.strftime("%Y年%m月%d日")
     end
+    binding.pry
   end
   
 end
