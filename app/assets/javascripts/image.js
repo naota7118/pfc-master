b286a2bda22595bfdf927eca1693b22f470ca0b1
naota7118 <naota7118@gmail.com>
2020-07-20 18:55:39 +0900

    edit post.rb

--- app/assets/javascripts/image.js
+++ app/assets/javascripts/image.js
@@ -1,8 +1,17 @@
 $(function(){
 
-  $(document).on('change', '#post_image', function() { 
-    console.log('hello');
+  // file_fieldのnameに動的なindexをつける為の配列
+  let fileIndex = [1,2,3,4,5,6,7,8,9,10,11,12];
 
-  });
+  lastIndex = $('.js-file_group:last').data('index');
+  fileIndex.splice(0, lastIndex);
+
+  $(document).on('change', '#post_image', function() { 
+
+    $('.hidden-destroy').hide();
+    // fileIndexの先頭の数字を使ってinputを作る  
+    $('.hidden-content').append(buildFileField(fileIndex[0]));
+    
+  })
 
 });
