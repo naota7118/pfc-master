3747a2d3b7cc79d16c8f266d60c516b9970efa79
naota7118 <naota7118@gmail.com>
2020-07-24 10:24:06 +0900

    setting images

---
+++ app/assets/javascripts/backgroudimage.js
@@ -0,0 +1,13 @@
+// $(window).load(function() {
+//   $('.bg-slider').bgSwitcher({
+//     images: ['images/Pumphose.muscles.png', 'images/allef-vinicius-do-0ghSDd2E-unsplash.jpg', 'images/sven-mieke-optBC2FxCfc.jpg'], // 切り替える背景画像を指定
+//     interval: 4000, // 背景画像を切り替える間隔を指定 3000=3秒
+//     loop: true, // 切り替えを繰り返すか指定 true=繰り返す　false=繰り返さない
+//     shuffle: true, // 背景画像の順番をシャッフルするか指定 true=する　false=しない
+//     effect: "fade", // エフェクトの種類をfade,blind,clip,slide,drop,hideから指定
+//     duration: 2000, // エフェクトの時間を指定します。
+//     easing: "swing" // エフェクトのイージングをlinear,swingから指定
+
+//   });
+
+// });
