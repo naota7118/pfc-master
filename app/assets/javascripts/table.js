f586a026396b59ef8d8444a6820af35d75d4f1ba
naota7118 <naota7118@gmail.com>
2020-07-30 14:36:56 +0900

    edit table.js

---
+++ app/assets/javascripts/table.js
@@ -0,0 +1,35 @@
+$(function(){
+
+  function buildHTML(){
+    var html = `<tr>
+                  <td>
+                    <input type="text" name="post[food]" id="post_food">
+                  </td>
+                  <td>
+                    <input type="number" name="post[calorie]" id="post_calorie">
+                  </td>
+                  <td>
+                    <input type="number" name="post[protein]" id="post_protein">
+                  </td>
+                  <td>
+                    <input type="number" name="post[fat]" id="post_fat">
+                  </td>
+                  <td>
+                    <input type="number" name="post[carbo]" id="post_carbo">
+                  </td>
+                  <td class="icon-plus">
+                    <i class="fa fa-2x fa-plus-circle table-button"></i>
+                  </td>
+                  <td class="icon-minus">
+                    <i class="fa fa-2x fa-minus-circle table-button"></i>
+                  </td>
+                </tr>`
+    return html;
+  }
+
+  $('.table-button').click(function() {
+    console.log('hello');
+    var html = buildHTML();
+    $('.table-body').append(html);
+  })
+});
