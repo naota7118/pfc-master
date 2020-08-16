199d1a656e8c06421e13d45d8c5348c3a85253f1
naota7118 <naota7118@gmail.com>
2020-08-16 21:38:18 +0900

    add image column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_03_212001) do
+ActiveRecord::Schema.define(version: 2020_08_16_120835) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "comment", null: false
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2020_08_03_212001) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.datetime "start_time"
+    t.string "image"
   end
 
   create_table "relationships", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
