8acce9692da0cf964e5e7feaddd1d77cacef714f
naota7118 <naota7118@gmail.com>
2020-08-20 13:35:16 +0900

    change preview image design

--- app/assets/javascripts/image-preview.js
+++ app/assets/javascripts/image-preview.js
@@ -25,20 +25,35 @@ $(function() {
       reader.readAsDataURL(file); 
       //読み込み時に発火するイベント onloadメソッドは読み込みが完了したら実行する
       reader.onload = function() {
-        console.log('hoge');
         //直前に実行したイベントが返した値を取得する
         var image = this.result;
+
+        // プレビュー画像がまだ場合
+        if($('.preview-box').length == 0){
+          // プレビュー用のhtmlを追加
+          var html = buildHTML();      
+          $('.icon').before(html);
+  
+          //画像を追加
+          $(`.upper-box img`).attr('src', `${image}`);
+
+        // すでにプレビュー画像が存在する場合
+        }else{
+          // プレビュー画像を削除
+        $('.preview-box').remove();
+
         // プレビュー用のhtmlを追加
-        var html = buildHTML();
-        
+        var html = buildHTML();      
         $('.icon').before(html);
 
         //画像を追加
         $(`.upper-box img`).attr('src', `${image}`);
+        }
+
       }
     })
 
-    // プレビュー画像を削除
+    // 「削除」を押すと削除イベントが発火する
     $(document).on("click", '.delete-box', function(){
       // プレビュー画像を削除
       $('.preview-box').remove();
