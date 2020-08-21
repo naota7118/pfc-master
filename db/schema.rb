b9ec6359b6b565138b3114f52083931c7207885e
naota7118 <naota7118@gmail.com>
2020-08-21 15:30:45 +0900

    change column float

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_21_060137) do
+ActiveRecord::Schema.define(version: 2020_08_21_062015) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "comment", null: false
@@ -63,16 +63,16 @@ ActiveRecord::Schema.define(version: 2020_08_21_060137) do
   end
 
   create_table "standards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "weight"
-    t.integer "calorie"
+    t.float "weight"
+    t.float "calorie"
     t.integer "protein"
-    t.integer "fat"
-    t.integer "carbo"
+    t.float "fat"
+    t.float "carbo"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "bodyFatPercentage"
-    t.integer "leanBodyMass"
+    t.float "bodyFatPercentage"
+    t.float "leanBodyMass"
     t.index ["user_id"], name: "index_standards_on_user_id"
   end
 
