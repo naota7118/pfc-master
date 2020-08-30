34820f460d0552492091914c66165d0c0a549bc8
naota7118 <naota7118@gmail.com>
2020-08-31 06:05:40 +0900

    change data_type to bigint

---
+++ db/migrate/20200830205027_change_data_user_id_to_comments.rb
@@ -0,0 +1,5 @@
+class ChangeDataUserIdToComments < ActiveRecord::Migration[5.2]
+  def change
+    change_column :comments, :user_id, :bigint
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_21_065248) do
+ActiveRecord::Schema.define(version: 2020_08_30_205027) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "comment", null: false
