de88bb528b596f09b8083bb7ac09eded2cac9055
naota7118 <naota7118@gmail.com>
2020-09-12 21:40:41 +0900

    rails db:migrate reset

--- db/migrate/20200803212001_change_column_to_allownull.rb
+++
@@ -1,9 +0,0 @@
-class ChangeColumnToAllownull < ActiveRecord::Migration[5.2]
-  def up
-    change_column :images, :image,:string, null: true # null: trueを明示する必要がある
-  end
-
-  def down
-    change_column :images, :image,:string, null: false
-  end
-end
