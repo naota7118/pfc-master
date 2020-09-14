0bb66f093b675650c7dcb2bbdfede0d6b712057e
naota7118 <naota7118@gmail.com>
2020-09-14 22:08:08 +0000

    edit posts controller index action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:show, :create]
   
   def index
-    @posts = Post.all.includes(:user)
+    @posts = Post.all.includes(:user).order("created_at DESC").page(params[:page]).per(5)
     @post = Post.new
     @user = current_user
     # 今日の合計カロリー
