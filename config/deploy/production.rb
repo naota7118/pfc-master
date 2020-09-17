3fe32d093123c30e45a972bc3ca38077b629a77c
naota7118 <naota7118@gmail.com>
2020-09-17 19:44:42 +0900

    edit deploy.rb and production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,9 @@
+# EC2サーバーのIP、EC2サーバーにログインするユーザー名、サーバーのロールを記述
+server '13.113.23.81', user: 'naota', roles: %w{app db web} 
+
+#デプロイするサーバーにsshログインする鍵の情報を記述
+set :ssh_options, keys: '~/.ssh/pfcmaster7118_aws_rsa' 
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
