3437087e5fdf7f05e1863cf8273d99058c31324b
naota7118 <naota7118@gmail.com>
2020-09-18 20:39:37 +0900

    update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ set :branch, 'master'
 set :deploy_to, '/var/www/pfc-master'
 
 # シンボリックリンクをはるファイル。(※後述)
-# set :linked_files, fetch(:linked_files, []).push('config/settings.yml')
+set :linked_files, fetch(:linked_files, []).push("config/master.key")
 
 # シンボリックリンクをはるフォルダ。(※後述)
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
