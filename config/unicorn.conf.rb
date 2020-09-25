2b6aed0b7d90bc4383f4c713f0b3fca090bb9ca2
naota7118 <naota7118@gmail.com>
2020-09-25 14:27:52 +0900

    edit unicorn

--- config/unicorn.conf.rb
+++ config/unicorn.conf.rb
@@ -1,20 +1,11 @@
-app_path = File.expand_path('../../../', __FILE__)
-worker_processes 1
-
-# set lets
-# $worker  = 2
+$worker  = 2
 $timeout = 30
-# $app_dir = "/var/www/pfc-master/current"
-# $listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
-$listen  = "#{app_path}/shared/tmp/sockets/unicorn.sock"
-# $pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
-$pid     = "#{app_path}/shared/tmp/pids/unicorn.pid"
-$std_log = "#{app_path}/shared/log/unicorn.stderr.log"
-$std_log = "#{app_path}/shared/log/unicorn.stdout.log"
-# set config
-# worker_processes  $worker
-# working_directory $app_dir
-working_directory "#{app_path}/current"
+$app_dir = "/var/www/pfc-master/current"
+$listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
+$pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
+$std_log = File.expand_path 'log/unicorn.log', $app_dir
+worker_processes  $worker
+working_directory $app_dir
 stderr_path $std_log
 stdout_path $std_log
 timeout $timeout
