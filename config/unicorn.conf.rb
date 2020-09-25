a4c884e8d15cb2110b36c64ede458f6a8697e5f3
naota7118 <naota7118@gmail.com>
2020-09-25 14:17:28 +0900

    modify app_dir path

--- config/unicorn.conf.rb
+++ config/unicorn.conf.rb
@@ -2,9 +2,9 @@ app_path = File.expand_path('../../../', __FILE__)
 worker_processes 1
 
 # set lets
-$worker  = 2
+# $worker  = 2
 $timeout = 30
-$app_dir = "/var/www/pfc-master/current"
+# $app_dir = "/var/www/pfc-master/current"
 # $listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
 $listen  = "#{app_path}/shared/tmp/sockets/unicorn.sock"
 # $pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
@@ -12,7 +12,7 @@ $pid     = "#{app_path}/shared/tmp/pids/unicorn.pid"
 $std_log = "#{app_path}/shared/log/unicorn.stderr.log"
 $std_log = "#{app_path}/shared/log/unicorn.stdout.log"
 # set config
-worker_processes  $worker
+# worker_processes  $worker
 # working_directory $app_dir
 working_directory "#{app_path}/current"
 stderr_path $std_log
