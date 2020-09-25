8af7aeb0afdc43b1d0fc5cb83f84209902906ba2
naota7118 <naota7118@gmail.com>
2020-09-25 13:18:10 +0900

    edit unicorn.conf.rb

--- config/unicorn.conf.rb
+++ config/unicorn.conf.rb
@@ -1,13 +1,20 @@
+app_path = File.expand_path('../../../', __FILE__)
+worker_processes 1
+
 # set lets
 $worker  = 2
 $timeout = 30
-$app_dir = "/var/www/rails/current" #自分のアプリケーション名
-$listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
-$pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
-$std_log = File.expand_path 'log/unicorn.log', $app_dir
+$app_dir = "/var/www/pfc-master/current"
+# $listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
+$listen  = "#{app_path}/shared/tmp/sockets/unicorn.sock"
+# $pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
+$pid     = "#{app_path}/shared/tmp/pids/unicorn.pid"
+$std_log = "#{app_path}/shared/log/unicorn.stderr.log"
+$std_log = "#{app_path}/shared/log/unicorn.stdout.log"
 # set config
 worker_processes  $worker
-working_directory $app_dir
+# working_directory $app_dir
+working_directory "#{app_path}/current"
 stderr_path $std_log
 stdout_path $std_log
 timeout $timeout
