5beacde1de49e60e75fa29a5f84bef88e16334de
naota7118 <naota7118@gmail.com>
2020-09-27 08:40:58 +0900

    change

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,9 @@ class User < ApplicationRecord
   has_many :followers, through: :follower_relationships, source: :follower
   has_one :standard
 
+  validates :name, presence: true
+  validates :email, presence: true
+
   def already_liked?(post)
     self.likes.exists?(post_id: post.id)
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do 
+    @user = build(:user)
+  end
+
+  describe 'バリデーション' do
+    it 'nameとemailどちらも値が設定されていれば、OK' do
+      expect(@user.valid?).to eq(true)
+    end
+
+    it 'nameが空だとNG' do
+      @user.name = ''
+      expect(@user.valid?).to eq(false)
+    end
+
+    it 'emailが空だとNG' do
+      @user.email = ''
+      expect(@user.valid?).to eq(false)
+    end
+  end
 end
