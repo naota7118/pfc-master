db9d7de6d4253862e7f4da6e2e727702db2922a8
naota7118 <naota7118@gmail.com>
2020-09-27 06:15:45 +0900

    change

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,6 +1,6 @@
 $worker  = 2
 $timeout = 30
-$app_dir = "/var/www/pfc-master/current"
+$app_dir = "/var/www/rails/pfc-master"
 $listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
 $pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
 $std_log = File.expand_path 'log/unicorn.log', $app_dir
