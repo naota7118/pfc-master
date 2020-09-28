8908244592539a30fb16200f561bb3f81f6514ee
naota7118 <naota7118@gmail.com>
2020-09-28 23:58:12 +0900

    change

--- config/application.rb
+++ config/application.rb
@@ -2,14 +2,6 @@ require_relative 'boot'
 
 require 'rails/all'
 
-config.generators do |g|
-  g.test_framework :rspec, 
-        view_specs: false, 
-        helper_specs: false, 
-        controller_specs: false, 
-        routing_specs: false
-end
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
