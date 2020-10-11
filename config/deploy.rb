cb6bdfc28da210256f1a6514e5af62a061d7cf07
naota7118 <naota7118@gmail.com>
2020-10-12 07:04:14 +0900

    edit deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,11 +13,12 @@ set :repo_url,  'https://github.com/naota7118/pfc-master.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
+set :rbenv_ruby, '2.5.1'
+set :rbenv_custom_path, '/home/naota/.rbenv'
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/ssh pfcmaster7118_key_rsa'] 
+                  keys: ['~/.ssh/pfcmaster7118_key_rsa'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
