9743479cb57274e9ded37619bc71a14cc78ab259
naota7118 <naota7118@gmail.com>
2020-10-17 18:21:38 +0900

    change

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,7 +34,7 @@ set :linked_files, fetch(:linked_files, []).push("config/master.key")
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
-    invoke! 'unicorn:restart'
+    invoke 'unicorn:restart'
   end
 end
 
