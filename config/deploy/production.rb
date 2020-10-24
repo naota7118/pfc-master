6d1e6ce0e120924ce3d3b1a04e8e245fc5fd86ca
naota7118 <naota7118@gmail.com>
2020-10-24 19:35:31 +0900

    change

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -26,8 +26,6 @@ set :ssh_options, keys: "~/.ssh/pfcmaster7118_key_rsa"
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
 
-
-
 # Configuration
 # =============
 # You can set any configuration variable like in config/deploy.rb
