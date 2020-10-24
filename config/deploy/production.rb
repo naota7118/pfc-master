526b9683eb9e2cb0c6b673c637f97c8abe260d39
naota7118 <naota7118@gmail.com>
2020-10-24 13:05:39 +0900

    change

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,7 +5,6 @@ server "pfcmaster.work", user: "naota", roles: %w{app db web}
 # デプロイするサーバーにsshログインする鍵の情報を記述
 set :ssh_options, keys: "~/.ssh/pfcmaster7118_key_rsa"
 
-
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
