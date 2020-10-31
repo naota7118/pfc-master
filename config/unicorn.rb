d258074744d116b22e7743927a1f0ca23889500a
naota7118 <naota7118@gmail.com>
2020-11-01 01:01:39 +0900

    Revert "update config/unicorn.rb"
    This reverts commit 5e6f44fc86662f4cc17d27501b7894a1c4b090ad.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,55 +1,32 @@
-#サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
-app_path = File.expand_path('../../', __FILE__)
+# frozen_string_literal: true
 
-#アプリケーションサーバの性能を決定する
+# ../が一つ増えている
+app_path = File.expand_path("../../../", __FILE__)
+
 worker_processes 1
+# currentを指定
+working_directory "#{app_path}/current"
 
-#アプリケーションの設置されているディレクトリを指定
-working_directory app_path
+# それぞれ、sharedの中を参照するよう変更
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
-#Unicornの起動に必要なファイルの設置場所を指定
-pid "#{app_path}/tmp/pids/unicorn.pid"
-
-#ポート番号を指定
-listen 3000
-
-#エラーのログを記録するファイルを指定
-stderr_path "#{app_path}/log/unicorn.stderr.log"
-
-#通常のログを記録するファイルを指定
-stdout_path "#{app_path}/log/unicorn.stdout.log"
-
-#Railsアプリケーションの応答を待つ上限時間を設定
-timeout 60
-
-#以下は応用的な設定なので説明は割愛
-
+# loading booster
 preload_app true
-GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
-
-check_client_connection false
-
-run_once = true
-
+# before starting processes
 before_fork do |server, worker|
-  defined?(ActiveRecord::Base) &&
-    ActiveRecord::Base.connection.disconnect!
-
-  if run_once
-    run_once = false # prevent from firing again
-  end
-
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
   old_pid = "#{server.config[:pid]}.oldbin"
-  if File.exist?(old_pid) && server.pid != old_pid
+  if old_pid != server.pid
     begin
-      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
-      Process.kill(sig, File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH => e
-      logger.error e
+      Process.kill "QUIT", File.read(old_pid).to_i
+    rescue Errno::ENOENT, Errno::ESRCH
     end
   end
 end
-
-after_fork do |_server, _worker|
-  defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
+# after finishing processes
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
 end
