32cd7e9c785c7ba75e8ad3fe959accb580ad5b0a
naota7118 <naota7118@gmail.com>
2020-11-02 23:26:33 +0900

    update config/unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,12 +1,25 @@
-# ../が一つ増えている
-app_path = File.expand_path('../../../', __FILE__)
+# 以下はunicornを使った手動でのデプロイの時に使う
+app_path = File.expand_path('../../', __FILE__)
 
 worker_processes 1
-# currentを指定
-working_directory "#{app_path}/current"
 
-# それぞれ、sharedの中を参照するよう変更
-listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
-pid "#{app_path}/shared/tmp/pids/unicorn.pid"
-stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
-stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
+working_directory app_path
+pid "#{app_path}/tmp/pids/unicorn.pid"
+listen "#{app_path}/tmp/sockets/unicorn.sock"
+stderr_path "#{app_path}/log/unicorn.stderr.log"
+stdout_path "#{app_path}/log/unicorn.stdout.log"
+
+# 以下はCapistranoの自動デプロイの時に使う
+
+# # ../が一つ増えている
+# app_path = File.expand_path('../../../', __FILE__)
+
+# worker_processes 1
+# # currentを指定
+# working_directory "#{app_path}/current"
+
+# # それぞれ、sharedの中を参照するよう変更
+# listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+# pid "#{app_path}/shared/tmp/pids/unicorn.pid"
+# stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
+# stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
