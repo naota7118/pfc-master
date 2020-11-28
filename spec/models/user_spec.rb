cf8c609c36d3f0913b933f0e4d35fdaddbeda244
naota7118 <naota7118@gmail.com>
2020-11-28 23:03:46 +0900

    update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe User, type: :model do
 
   describe '#create' do
     it "メールアドレスがない場合は登録できないこと" do
-      user = User.new(name: "", email: "", password: "000000", password_confirmation: "000000")
+      user = User.new(name: "aaa", email: "", password: "000000", password_confirmation: "000000")
       user.valid?
       expect(user.errors[:email]).to include("can't be blank")
     end
