5f691674a61c0b3bd07ff18d4e378df3c11599a3
naota7118 <naota7118@gmail.com>
2020-12-22 23:52:00 +0900

    modify post_spec.rb

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe Post, type: :model do
     # john = create(:john)
     # curry = create(:curry, user_id: john.id)
     # post = Post.new(food: "カレー", calorie: '100',id: '1', protein: nil, fat: nil, carbo: nil, text: nil, image: nil, created_at: '2020-12-20', updated_at: '2020-12-20', user_id: 1, weight: nil, start_time: '2020-12-20')
-    post = Post.new(id: nil, food: "カレー", calorie: '500', protein: nil, fat: nil, carbo: nil, text: nil, image: nil, created_at: nil, updated_at: nil, user_id: nil, weight: nil, start_time: nil)
+    post = Post.new(id: nil, food: "カレー", calorie: 500, protein: nil, fat: nil, carbo: nil, text: nil, image: nil, created_at: nil, updated_at: nil, user_id: nil, weight: nil, start_time: nil)
     expect(post).to be_valid
   end
 
