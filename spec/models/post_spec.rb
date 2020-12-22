1337ebe8c6bc8d059edced98683a06c69e762c97
naota7118 <naota7118@gmail.com>
2020-12-22 21:05:23 +0900

    modify post_spec.rb

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe Post, type: :model do
   it "foodとcalorieがあれば投稿できる" do
     # john = create(:john)
     # curry = create(:curry, user_id: john.id)
-    post = Post.new(food: "カレー", calorie: '100', protein: nil, fat: nil, carbo: nil, text: nil, image: nil, created_at: '2020-12-20', updated_at: '2020-12-20', user_id: 1, weight: nil, start_time: '2020-12-20')
+    post = Post.new(food: "カレー", calorie: '100',id: '1', protein: nil, fat: nil, carbo: nil, text: nil, image: nil, created_at: '2020-12-20', updated_at: '2020-12-20', user_id: 1, weight: nil, start_time: '2020-12-20')
     expect(post).to be_valid
   end
 
